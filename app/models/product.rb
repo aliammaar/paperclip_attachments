6316f97b54113a00ff87a8be882df7af4587e4b0
Ali Ammaar <alie.ammaar@gmail.com>
2021-10-09 20:39:24 +0530

    paperclip attachments working

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ApplicationRecord
+  has_attached_file :image
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
+  # validates :image, attachment_presence: true
+  # validates_with AttachmentPresenceValidator, attributes: :image
+  # validates_with AttachmentSizeValidator, attributes: :image, less_than: 1.megabytes
+  # validates_attachment :image, content_type: ["image/jpeg", "image/gif", "image/png"]
+
 end
